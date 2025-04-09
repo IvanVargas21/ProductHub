5864e4d88f127144efbcfe7bc59f7be6d612d0cf
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-04-10 07:59:10 +0800

    Added: Project Dependencies

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config({path: '../.env'});
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log('../process.env.PORT');
+})
