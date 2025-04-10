9555edc3c9f34e10effdab1164bd5b6455da50b5
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-04-10 23:06:46 +0800

    Added: Project Initial Setup Done!

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
-dotenv.config({path: '../.env'});
-
+dotenv.config();
+// Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Parse incoming data
+app.use(express.json());
+app.use(cors());  
+app.use(morgan('dev')); // Log the requests to console
+app.use(helmet()); // Use Helmet middleware = adds security headers
+
+
+app.get('/test-route', (req, res) => {
+		// Log all headers that will be sent in the response
+    console.log(res.getHeaders())
+    res.send('Hello from Test-route!');
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    console.log('../process.env.PORT');
+    console.log(`Server is running on http://localhost: ${PORT}`);
 })
