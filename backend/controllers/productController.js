9abc2f1e3b3a9511f4ce01d1c8c97125be7812d8
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-04-12 19:15:40 +0800

    Added: Server Controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -13,7 +13,7 @@ export const getProducts = async (req, res) => {
         data: products,
       })
     }catch(error){
-        console.log("Error in getProducts", error)
+        console.log("Error in getProducts function", error)
         res.status(500).json({
           message: "Error getting products",
           error: error.message,
@@ -32,18 +32,123 @@ export const createProduct = async (req, res) => {
   }
 
   try {
-
+    // Why Array?
+    // sql function, designed to handle queries that may return multiple rows.
+    // each row is an object.
+    const newProduct = await sql `
+      INSERT INTO products (name, image, price)
+      VALUES (${name}, ${image}, ${price})
+      RETURNING *
+      `
+      console.log("New product added ")
+      res.status(201).json({
+        success: true,
+        message: "New Product Created Successfully",
+        data: newProduct[0],
+      })
   }catch(error){
-    console.log("Error in Create Product", error);
+    console.log("Error in createProduct function", error);
     res.status(500).json({
       success: false,
-      message: "Create Product Failed",
+      message: "Error creating new product",
       error: error.message,
     })
   }
 
 }
 
-export const getProduct = async (req, res) => {}
-export const updateProduct = async (req, res) => {}
-export const deleteProduct = async (req, res) => {}
+export const getProduct = async (req, res) => {
+  const {id} = req. params;
+
+  try {
+    const product = await sql `
+      SELECT * FROM products WHERE id=${id}
+      `
+      res.status(200).json({
+        succes: true,
+        message:" Get Product Success",
+        data: product[0]
+      })
+  }catch(error){
+    console.log("Error in getProduct function", error);
+    res.status(500).json({
+      success: false, 
+      message: "Error getting product",
+      error: error.message,
+    })
+  }
+}
+
+// RETURNING * - used ot return the values of the affected row(s) aftern an INSERT, UPDATE, or DELETE operation.
+export const updateProduct = async (req, res) => {
+  const {id} = req.params;
+  const {name, image, price} = req.body;
+
+  // if(!name || !image || !price) {
+  //   return res.status(400).json({
+  //     success: false,
+  //     message: "Update Product Failed, All Fields are Required!",
+  //   })
+  // }
+
+  try{
+    const updateProduct = await sql `
+      UPDATE products
+      SET name=${name}, image=${image}, price=${price}
+      WHERE id=${id}
+      RETURNING *
+    `;
+    
+    // if updateProduct[] = 0, not found
+    if(updateProduct.length === 0) {
+      return res.status(404).json({
+        success: false, 
+        message: "Product not found",
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Update Product Success",
+      data: updateProduct[0],
+    })
+  }catch(error){
+    console.log("Error in updateProduct function", error);
+    res.status(500).json({
+      success: false,
+      message: "Error updating product",
+    })
+  }
+}
+export const deleteProduct = async (req, res) => {
+  const {id} = req.params;
+
+  try {
+    const deletedProduct = await sql `
+      DELETE FROM products 
+      WHERE id=${id}
+      RETURNING *
+      `;
+
+      // if deletedProduct[] = 0, not found
+      if(deletedProduct.length === 0){
+        return res.status(404).json({
+          success: false,
+          message: "Product not found"
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Delete Product Success",
+        data: deletedProduct[0],
+      })
+  }catch(error){
+    console.log("Error in deleteProduct function", error);
+    res.status(500).json({
+      success: false,
+      message: "Error deleting product",
+      error: error.message,
+    })
+  }
+}
