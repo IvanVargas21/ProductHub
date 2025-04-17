49b2e3af35b84f8b11f97cc03c5a256fa5cb5dc0
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-04-17 14:39:53 +0800

    Added: Add Product Functionality

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -8,7 +8,38 @@ export const useProductStore = create ((set, get) => ({
     products: [],
     loading: false, 
     error: null, 
-		
+
+		formData: {
+			name: "",
+			price: "",
+			image: ""
+		},
+		setFormData: (formData)=> set({formData}),
+		resetForm: set({formData: {name: "", price: "", image: ""}}),
+
+		addProduct: async (e) => {
+			// prevent default behaviour in an event in js
+			// default behaviour is to refresh the page when a form is submitted
+			// this is not what we want in this case
+			// allows us to handle the form submission with js
+			e.preventDefault();
+			set({ loading: true });
+	
+			try {
+				const { formData } = get();
+				await axios.post(`${BASE_URL}/api/products`, formData);
+				await get().fetchProducts();
+				get().resetForm();
+				toast.success("Product added successfully");
+				document.getElementById("add_product_modal").close();
+			} catch (error) {
+				console.log("Error in addProduct function", error);
+				toast.error("Something went wrong");
+			} finally {
+				set({ loading: false });
+			}
+		},
+
     fetchProducts: async () => {
 			set({ loading: true});
 			try {
